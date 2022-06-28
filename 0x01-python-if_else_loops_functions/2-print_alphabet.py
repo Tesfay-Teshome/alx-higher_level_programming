20b7c39d6fa836d6763ee3d23da03f74ea00597f
Tesfay-Teshome <tesfix2017@gmail.com>
2022-06-28 03:17:44 -0700

    Task 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """Print the alphabet in lowrcase, not followed by new line."""
 for letter in range(97, 123):
-    print("{}".format(chr(letter)), end=""))
+    print("{}".format(chr(letter)), end="")
