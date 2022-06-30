1abdf05f361d6361690bb7d1e0eb140736bf59bc
Tesfay-Teshome <tesfix2017@gmail.com>
2022-06-30 08:29:17 -0700

    Calculator 1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Return:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """ My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Retruns:
+         The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """ The Multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """ The Division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return (a / b)
