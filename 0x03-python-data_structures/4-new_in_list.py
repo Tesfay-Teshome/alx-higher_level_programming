3ab8f2de767cae02f6259d204ffe6c57a89a53dc
Tesfay-Teshome <tesfix2017@gmail.com>
2022-07-02 00:28:40 -0700

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) -1):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
     copy = [x for x in my_list]
