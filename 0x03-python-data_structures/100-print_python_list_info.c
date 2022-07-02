9f8d567566c8a54a3d37023039852e72d81c5905
Tesfay-Teshome <tesfix2017@gmail.com>
2022-07-02 08:01:50 +0000

    Task 14 from Ubuntu 14.04

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,8 @@
 #include <Python.h>
 
 /**
- * print_python_list_info - Prints basic info about Python lists.
- * @p: A PyObject list.
+ * print_python_list_info - Prints basic info about my Python lists.
+ * @p: A Py-Object list.
  */
 void print_python_list_info(PyObject *p)
 {
