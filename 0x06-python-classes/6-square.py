f20025de635b56546581a28d7f94585aed193984
Tesfay-Teshome <tesfix2017@gmail.com>
2022-07-19 05:54:34 -0700

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,83 +1,55 @@
 #!/usr/bin/python3
-"""My square module"""
-
-
 class Square:
-    """defines a square"""
+
+    """ Class Square that defines methods and attributes for a square object"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Create a Square
-        Args:
-            size: length of a side of Square
-            position: where the square is (coordinates)
-        """
+        """ Class Constructor"""
         self.size = size
         self.position = position
 
-    def __str__(self):
-        self.my_print()
-
     @property
     def size(self):
-        """"The propery of size as the len of a side of Square
-        Raises:
-            TypeError: if size != int
-            ValueError: if size < 0
-        """
-        return self.__size
+        """ Private Attribute size Getter """
+        return (self.__size)
 
     @size.setter
     def size(self, value):
+        """ Private Attribute size Setter """
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        """property of the coordinates of this Square
-        Raises:
-            TypeError: if value != a tuple of 2 integers < 0
-        """
-        return self.__position
+        """ Private Attribute position Getter """
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        """set the position of this Square
-        Args: value as a tuple of two positive integers
-        Raises:
-            TypeError: if value is not a tuple or any int in tuple < 0
-        """
-        if not isinstance(value, tuple):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = value
+        """ Private Attribute position Setter """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
-        """Get the area of a Square
-        Returns: The size squared
-        """
-        return self.__size * self.__size
-
-    def pos_print(self):
-        """returns the position in spaces"""
-        pos = ""
-        if self.size == 0:
-            return "\n"
-        for w in range(self.position[1]):
-                pos += "\n"
-        for w in range(self.size):
-            for l in range(self.position[0]):
-                pos += " "
-            for l in range(self.size):
-                pos += "#"
-            pos += "\n"
-        return pos
+        """ Method that calculates current square area """
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """print the square in position"""
-        print(self.pos_print(), end='')
+        """ Method that prints in stdout the square with the char # """
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.size):
+                print("{}{}".format(' '*self.__position[0], '#'*self.__size))
