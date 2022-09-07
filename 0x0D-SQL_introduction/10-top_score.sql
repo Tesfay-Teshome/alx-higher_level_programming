58233cc0688933a658715151da1412c3dcb741c9
Tesfay-Teshome <tesfix2017@gmail.com>
2022-09-07 01:33:53 -0700

    Top Score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
 -- Lists all records of the table second_table of the database
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
