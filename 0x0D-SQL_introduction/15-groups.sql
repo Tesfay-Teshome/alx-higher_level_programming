a8be6485f5d48f22d768bf3f06f5e9a6d0d0db1c
Tesfay-Teshome <tesfix2017@gmail.com>
2022-09-07 04:07:37 -0700

    Group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,2 @@
 -- Lists the number of records with the same score in the table second_table
-SELECT 'score', COUNT(*) AS 'number'
-FROM 'second_table'
-GROUP BY 'score'
-ORDER BY 'number' DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
