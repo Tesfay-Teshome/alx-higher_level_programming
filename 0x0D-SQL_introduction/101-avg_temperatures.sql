8c552c46b52f6831e0080525872df1cd4241f3c0
Tesfay-Teshome <tesfix2017@gmail.com>
2022-09-07 04:22:38 -0700

    Average Tempratures

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
+-- Query to perform operation
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+       GROUP BY city ORDER BY avg_temp DESC;
