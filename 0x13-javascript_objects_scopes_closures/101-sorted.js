c701a4e52dfe14f167f104714b82bc3261dcfc57
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-04 11:42:08 -0700

    Sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+const dict = require('./101-data').dict;
+const newDict = {};
+
+for (const key in dict) {
+  if (typeof (newDict[dict[key]]) === 'undefined') {
+    newDict[dict[key]] = [];
   }
+  newDict[dict[key]].push(key);
 }
-console.log(newDict)
+
+console.log(newDict);
