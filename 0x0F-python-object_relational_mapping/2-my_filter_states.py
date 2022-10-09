a8baab830884ebd5ab1edf60c51d038029645fd7
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-09 04:12:32 -0700

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+!/usr/bin/python3
+""" Filter states by user input """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    cur = db.cursor()
+
+    query = """
+    SELECT states.id, name FROM states WHERE name='{:s}'
+    COLLATE latin1_general_cs
+    ORDER BY states.id ASC;
+    """.format(state_name)
+
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
