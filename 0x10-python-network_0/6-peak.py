3bc6f22ae8686f9de4e640514e6307f41d3d5aaa
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-20 10:06:16 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
+""" Function to find a peak in list """
+
+
+def peak_recur(a, Min, Max):
+    """ Binary Search Recursion """
+
+    if not a:
+        return None
+
+    half = int((Max + Min) / 2)
+
+    if (half - 1) < Min and (half + 1) > Max:
+        return a[half]
+
+    if (half - 1) < Min and (half + 1) <= Max and a[half + 1] <= a[half]:
+        return a[half]
+
+    if (half + 1) > Max and (half - 1) >= Min and a[half - 1] <= a[half]:
+        return a[half]
+
+    if (half - 1) >= Min and a[half - 1] <= a[half] \
+       and (half + 1) <= Max and a[half + 1] <= a[half]:
+        return a[half]
+
+    if a[half + 1] > a[half]:
+        return peak_recur(a, half + 1, Max)
+
+    return peak_recur(a, 0, half - 1)
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """ Finds a peak in unsorted array """
+
+    if not list_of_integers:
         return None
-    size = len(list_of_integers)
 
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    Min = 0
+    Max = len(list_of_integers) - 1
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    return peak_recur(list_of_integers, Min, Max)
