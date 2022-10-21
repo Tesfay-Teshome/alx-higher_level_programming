82245e4193219a32ca307494709642d2ceb8a000
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-21 10:27:13 -0700

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" What's my status? """
+
+if __name__ == "__main__":
+    import urllib.request
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
